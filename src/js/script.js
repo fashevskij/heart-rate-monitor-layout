ca967672c188e38195051e22d2f740390fa03b61
fashevskij <68337800+fashevskij@users.noreply.github.com>
2020-12-08 19:57:41 +0200

    add modal window

--- src/js/script.js
+++ src/js/script.js
@@ -19,6 +19,24 @@ $(document).ready(function () {
     }
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //modal window
+
+    $("[data-modal=consultation]").on('click',function(){
+        $('.overlay, #consultation').fadeIn('slow');
+
+    });
+    $('.modals__close').on('click',function (){
+        $(".overlay, #consultation,#thanks,#order").fadeOut('slow');
+    });
+
+
+    $('.button_mini').each(function (i){
+        $(this).on('click',function(){
+            $('#order .modals__desc').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
 
 
